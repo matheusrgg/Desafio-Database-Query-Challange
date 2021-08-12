baa8e52479a5204e0b9e3c18f071d88d255c1df4
matheusrgg <matheusrgg@gmail.com>
2021-08-12 17:59:35 -0300

    first commit

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return this.repository
+      .createQueryBuilder('game')
+      .where("LOWER(game.title) ILIKE :title", { title: `%${param}%` })
+      .getMany();
+
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+
+    return this.repository.query('SELECT COUNT (*) FROM games'); // Complete usando raw query
+
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    const { users } = await this.repository
+      .createQueryBuilder('game')
+      .innerJoinAndSelect('game.users', 'users')
+      .where('game.id = :id', { id })
+      .getOneOrFail();
+
+    return users;
+  }
+}
